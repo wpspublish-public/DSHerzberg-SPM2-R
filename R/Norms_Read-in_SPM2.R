feedd3c857772ca9afacf8bb9078de0e32c90f01
David Herzberg <dherzberg@wpspublish.com>
2019-03-07 17:02:21 -0800

    item numbers

--- R/Norms_Read-in_SPM2.R
+++ R/Norms_Read-in_SPM2.R
@@ -10,8 +10,23 @@ suppressMessages(suppressWarnings(library(tidyverse)))
 suppressMessages(library(ggpmisc)) # EXTENSIONS TO ggplot2: ADD EQUATIONS AND FIT STATISTICS TO FITTED LINE PLOTS
 library(ggrepel) # MORE ggplot2 EXTENSIONS
 
+TOT_items_Adult_Self <- c("q0013", "q0014", "q0016", "q0018", "q0020", "q0021", "q0023", 
+                          "q0024", "q0026", "q0027", "q0029", "q0030", "q0031", "q0032", 
+                          "q0033", "q0034", "q0036", "q0037", "q0040", "q0041", "q0043", 
+                          "q0044", "q0046", "q0047", "q0049", "q0050", "q0051", "q0053", 
+                          "q0054", "q0057", "q0058", "q0059", "q0060", "q0061", "q0062", 
+                          "q0063", "q0064", "q0067", "q0068", "q0069", "q0073", "q0075", 
+                          "q0076", "q0077", "q0081", "q0082", "q0083", "q0084", "q0086", 
+                          "q0087", "q0089", "q0090", "q0091", "q0092", "q0093", "q0096", 
+                          "q0098", "q0099", "q0100", "q0101", "q0103", "q0104", "q0105", 
+                          "q0106", "q0108", "q0111", "q0113", "q0114", "q0115", "q0117", 
+                          "q0118", "q0119", "q0122", "q0126", "q0127", "q0128", "q0129", 
+                          "q0130", "q0131", "q0132")
+
 Adult_Self <-
-  suppressMessages(read_csv(here('DATA/SPM-2 Adult ages 1690 Self-Report Questionnaire.csv')))
+  suppressMessages(print(as_tibble(read_csv(
+    here('DATA/SPM-2 Adult ages 1690 Self-Report Questionnaire.csv')
+  )))) %>% select(IDNumber, Age, AgeGroup, Gender, HighestEducation, Ethnicity, Region, TOT_items_Adult_Self)
 
 # Check for duplicate IDnumber.
 

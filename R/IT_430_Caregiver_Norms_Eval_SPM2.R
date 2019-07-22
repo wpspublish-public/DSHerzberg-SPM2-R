1b9decd9465ff47b89418fb14fc478e12a8d383b
David Herzberg <dherzberg@wpspublish.com>
2019-07-22 15:48:49 -0700

    Rescore TOT scale

--- R/IT_430_Caregiver_Norms_Eval_SPM2.R
+++ R/IT_430_Caregiver_Norms_Eval_SPM2.R
@@ -11,14 +11,13 @@ library(ggrepel) # MORE ggplot2 EXTENSIONS
 
 # Scale vectors with item names
 
-TOT_items_IT_430_Caregiver <- c("QT1", "QT2", "QT4", "QT5", "QT6", "QT7", "QT10", "QT11", "QT12", "QT13", "QT14", "QT15", 
+TOT_items_IT_430_Caregiver <- c("QT14", "QT15", 
                                 "QT17", "QT18", "QT19", "QT20", "QT21", "QT23", "QT25", "QT26", "QT27", "QT28", "QT30", 
                                 "QT31", "QT32", "QT33", "QT34", "QT35", "QT37", "QT39", "QT41", "QT43", "QT45", "QT46", 
                                 "QT47", "QT48", "QT49", "QT50", "QT51", "QT54", "QT55", "QT56", "QT57", "QT60", "QT62", 
                                 "QT65", "QT66", "QT67", "QT68", "QT69", "QT71", "QT72", "QT73", "QT74", "QT75", "QT76", 
                                 "QT77", "QT78", "QT79", "QT81", "QT83", "QT85", "QT86", "QT87", "QT89", "QT91", "QT92", 
-                                "QT93", "QT94", "QT95", "QT96", "QT97", "QT98", "QT100", "QT101", "QT103", "QT105", 
-                                "QT106", "QT107", "QT108")
+                                "QT93", "QT94", "QT95")
 
 SOC_items_IT_430_Caregiver <- c("QT1", "QT2", "QT4", "QT5", "QT6", "QT7", "QT10", "QT11", "QT12", "QT13")
 

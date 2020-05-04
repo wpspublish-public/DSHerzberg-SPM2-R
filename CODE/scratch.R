7e1f0be10df3c40bb575b4c7c81b358f9902b051
David Herzberg <dherzberg@wpspublish.com>
2020-05-03 21:52:19 -0700

    trt ES

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,108 +1,100 @@
-# ADULT OTHER DATA ------------------------------------------------------------
+# LOAD PACKAGES -----------------------------------------------------------
+suppressMessages(library(here)) 
+suppressMessages(suppressWarnings(library(tidyverse)))
+suppressMessages(library(psych))
 
-source(here('CODE/ITEM-VECTORS/Adult-Other-item-vectors.R'))
+# ADULT SELF DATA ---------------------------------------------
 
-Adult_Other_time2 <-
+# read stand
+
+Adult_Self_Stand <- 
   suppressMessages(as_tibble(read_csv(
-    here("INPUT-FILES/ADULT/TIME-2/SPM-2 Adult ages 1690 Other Report Questionnaire time2.csv")
-  ))) %>% select(
-    IDNumber,
-    Age,
-    Gender,
-    HighestEducation,
-    Ethnicity,
-    Region,
-    All_items_Adult_Other
-  ) %>%
-  # recode items from char to num (mutate_at applies funs to specific columns)
-  mutate_at(
-    All_items_Adult_Other,
-    ~ case_when(
-      .x == "Never" ~ 1,
-      .x == "Occasionally" ~ 2,
-      .x == "Frequently" ~ 3,
-      .x == "Always" ~ 4,
-      TRUE ~ NA_real_
-    )
-  ) %>%
-  # recode reverse-scored items
-  mutate_at(
-    SOC_rev_items_Adult_Other,
-    ~ case_when(.x == 4 ~ 1,
-                .x == 3 ~ 2,
-                .x == 2 ~ 3,
-                .x == 1 ~ 4,
-                TRUE ~ NA_real_)
-  ) %>%
-  # Convert scored item vars to integers
-  mutate_at(All_items_Adult_Other,
-            ~ as.integer(.x)) %>% 
-  # Add age_range var.
-  mutate(age_range = case_when(
-    Age <= 30 ~ "21.00 to 30.99 years",
-    between(Age, 31, 40) ~ "31.00 to 40.99 years",
-    between(Age, 41, 50) ~ "41.00 to 50.99 years",
-    between(Age, 51, 64) ~ "51.00 to 64.99 years",
-    TRUE ~ "65.00 to 99.99 years")) %>% 
-  # select(-AgeGroup) %>% 
-  # Compute raw scores. Note use of `rowSums(.[TOT_items_Child_512_Other])`: when used 
-  # within a pipe, you can pass a vector of column names to `base::rowSums`, but you
-  # must wrap the column vector in a column-subsetting expression: `.[]`, where the
-  # dot is a token for the data in the pipe.
-  mutate(
-    TOT_raw = rowSums(.[TOT_items_Adult_Other]),
-    SOC_raw = rowSums(.[SOC_items_Adult_Other]),
-    VIS_raw = rowSums(.[VIS_items_Adult_Other]),
-    HEA_raw = rowSums(.[HEA_items_Adult_Other]),
-    TOU_raw = rowSums(.[TOU_items_Adult_Other]),
-    TS_raw = rowSums(.[TS_items_Adult_Other]),
-    BOD_raw = rowSums(.[BOD_items_Adult_Other]),
-    BAL_raw = rowSums(.[BAL_items_Adult_Other]),
-    PLA_raw = rowSums(.[PLA_items_Adult_Other])
-  ) %>% 
-  # Create data var 
-  mutate(data = 'time2',
-         clin_status = 'typ',
-         clin_dx = NA
-  ) %>% 
-  select(IDNumber, Age, age_range, Gender:Region, data, clin_status, clin_dx, everything())
+    here("OUTPUT-FILES/ADULT/T-SCORES-PER-CASE/Adult-Self-T-Scores-per-case.csv")
+  ))) %>% 
+  select(IDNumber:clin_dx, contains('_NT'))
 
-# read raw-to-t lookup tables, create lookup cols by scale
-Adult_rawToT <-
+# read time 2
+
+Adult_Self_time2 <- 
   suppressMessages(as_tibble(read_csv(
-    here("OUTPUT-FILES/ADULT/RAW-T-LOOKUP-TABLES/Adult-Other-raw-T-lookup.csv")
-  )))
+    here("OUTPUT-FILES/ADULT/T-SCORES-PER-CASE/Adult-Self-time2-T-Scores-per-case.csv")
+  ))) %>% 
+  select(IDNumber, contains('_NT'))
 
-map_df(
-  score_names,
-  ~
-    Adult_rawToT %>%
-    select(raw,!!str_c(.x, '_NT')) %>%
-    rename(!!str_c(.x, "_raw") := raw) %>%
-    assign(str_c(.x, '_1690_lookup_col'), ., envir = .GlobalEnv)
-)
+Adult_Self_TRT <- Adult_Self_Stand %>% 
+  inner_join(
+    Adult_Self_time2,
+    by = 'IDNumber',
+    suffix = c('_t1', '_t2')
+  ) %>% 
+  mutate(TOT_NT_dif = abs(TOT_NT_t1 - TOT_NT_t2)) %>% 
+  select(TOT_NT_dif, everything()) %>% 
+  arrange(TOT_NT_dif) %>% 
+  filter(TOT_NT_dif < 10)
 
-# look up T scores with left_join, bind T score columns to main data set
-output_1690 <- map_dfc(score_names,
-                       ~
-                         Adult_Other_time2 %>% left_join(eval(as.name(
-                           str_c(.x, '_1690_lookup_col')
-                         )),
-                         by = str_c(.x, '_raw')) %>%
-                         select(!!str_c(.x, '_NT'))) %>%
-  bind_cols(Adult_Other_time2, .) %>% 
-  arrange(IDNumber)
+cor_cols <- Adult_Self_TRT %>% 
+  select(contains('_NT'))
 
-# write outuput for analysis
+cor_row <- c('TOT_NT_1-TOT_NT_2', 'SOC_NT_1-SOC_NT_2', 'VIS_NT_1-VIS_NT_2',
+             'HEA_NT_1-HEA_NT_2', 'TOU_NT_1-TOU_NT_2', 'TS_NT_1-TS_NT_2',
+             'BOD_NT_1-BOD_NT_2', 'BAL_NT_1-BAL_NT_2', 'PLA_NT_1-PLA_NT_2')
 
-write_csv(
-  output_1690,
-  here(
-    'OUTPUT-FILES/ADULT/T-SCORES-PER-CASE/Adult-Other-time2-T-Scores-per-case.csv'
-  ),
-  na = ''
-)
+scale_order <- c("SOC", "VIS", "HEA", "TOU", 
+                 "TS", "BOD", "BAL", "PLA", "TOT")
 
-rm(list = ls())
+Adult_Self_TRT_cor_table <- corr.test(cor_cols)[['ci']] %>% 
+  rownames_to_column(var = 'pair') %>% 
+  filter(pair %in% cor_row) %>% 
+  mutate(scale1 = str_sub(pair, 1, 3),
+         scale = str_replace(scale1, '_', '')
+  ) %>% 
+  mutate_if(is.numeric, ~ round(., 3)) %>%
+  arrange(match(scale, scale_order)) %>% 
+  mutate(form = case_when(
+    scale == 'SOC' ~ 'Adult Self', 
+    T ~ NA_character_)
+  ) %>% 
+  select(form, scale, r, p)
 
+Adult_Self_t1_T_desc <-
+  Adult_Self_TRT %>% 
+  select(contains('_NT_t1')) %>% 
+  describe(fast = T) %>%
+  rownames_to_column() %>% 
+  rename(scale = rowname)%>% 
+  mutate(scale1 = str_sub(scale, 1, 3),
+         scale = str_replace(scale1, '_', '')
+  ) %>% 
+  arrange(match(scale, scale_order)) %>% 
+  mutate(form = case_when(
+    scale =="SOC_raw" ~ "Self Form t1",
+    T ~ NA_character_
+  )) %>% 
+  select(form, scale, n, mean, sd) %>% 
+  rename(n = n,
+         mean_t1 = mean,
+         sd_t1 = sd)
 
+Adult_Self_t2_T_desc <-
+  Adult_Self_TRT %>% 
+  select(contains('_NT_t2')) %>% 
+  describe(fast = T) %>%
+  rownames_to_column() %>% 
+  rename(scale = rowname)%>% 
+  mutate(scale1 = str_sub(scale, 1, 3),
+         scale = str_replace(scale1, '_', '')
+  ) %>% 
+  arrange(match(scale, scale_order)) %>% 
+  select(scale, mean, sd) %>% 
+  rename(mean_t2 = mean,
+         sd_t2 = sd)
+
+Adult_Self_TRT_T_desc <- Adult_Self_t1_T_desc %>% 
+  full_join(
+    Adult_Self_t2_T_desc,
+    by = 'scale'
+  )  %>%
+  select(-form) %>% 
+  mutate(ES = abs((mean_t1 - mean_t2) / ((sd_t1 + sd_t2 / 2)))) %>%
+  mutate_at(vars(mean_t1, sd_t1, mean_t2, sd_t2, ES), ~
+              (round(., 2)))

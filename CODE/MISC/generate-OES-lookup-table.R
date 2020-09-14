9036e813a6be30a7767c47c959ae2d866ba7ee24
David Herzberg <dherzberg@wpspublish.com>
2020-09-14 16:48:40 -0700

    Complete OES lookup code; ready for formal documentation

--- CODE/MISC/generate-OES-lookup-table.R
+++ CODE/MISC/generate-OES-lookup-table.R
@@ -1,20 +1,11 @@
 suppressMessages(library(here))
 suppressMessages(library(tidyverse))
 
-form_order <- c("Infant", "Toddler", "Caregiver", "Preschool", 
-                "Child", "Adolescent", "Adult")
-environ_order <- c("Home", "Caregiver", "School", "Self", "Other")
-age_group_order <- c("4-6 months", "7-9 months", "10-20 months", 
-                     "21-30 months", "Not Specified", "2-4 years", "5 years", 
-                     "5-12 years", "12-21 years", "21-87 years")
-scale_order <- c("VIS", "HEA", "TOU", "TS", "BOD", "BAL", "TOT", "PLA", "SOC")
+scale_order <-
+  c("VIS", "HEA", "TOU", "TS", "BOD", "BAL", "TOT", "PLA", "SOC")
 
-min_max <- tibble(
-  age_group = all_of(age_group_order), 
-  min_age = c(4, 7, 10, 21, 4, 24, 60, 60, 144, 252), 
-  max_age = c(6, 9, 20, 30, 1055, 59, 71, 155, 263, 1055)
-)
-
+# create form_IDs dataframes that lay out rows and columns structure for final
+# output, in desired final sequence
 form_IDs <- tribble(
   ~file, ~form, ~environ, ~age_group,
   "IT-46-Home", "Infant", "Home", "4-6 months", 
@@ -53,98 +44,150 @@ form_IDs_driving <- tribble(
   "adult-driving-other", "Driving", "Other", "21-87 years"
 )
 
-percentile <- suppressMessages(read_csv(
-  here(
-    str_c("INPUT-FILES/OES-TABLES/t-percentile-lookup-4080T.csv")
-  )
-))
+# Decompose age group into min/max age in months. This dataframe has a lookup
+# structure that can be used with left_join(). Note use of unique() to pull
+# age_group values from the forms_IDs dataframe.
+min_max <- tibble(
+  age_group = unique(form_IDs$age_group), 
+  min_age = c(4, 7, 10, 21, 4, 24, 60, 60, 144, 252), 
+  max_age = c(6, 9, 20, 30, 1055, 59, 71, 155, 263, 1055)
+)
 
+percentile <- suppressMessages(read_csv(here(
+  str_c("INPUT-FILES/OES-TABLES/t-percentile-lookup-4080T.csv")
+)))
+
+# create vector of file names of lookup tables to be transformed and bound
+# together into a single output file for OES upload
 file_name <- map_chr(form_IDs$file, ~ str_c(.x, "-raw-T-lookup-4080T.csv"))
 
-lookup <- file_name %>% map(
-  ~ suppressMessages(read_csv(
-    here(
-      str_c("INPUT-FILES/OES-TABLES/", .x)
-    )
-  ))
-) %>% 
-  set_names(form_IDs$file) %>% 
-  bind_rows(.id = "file") %>% 
-  left_join(x = form_IDs, by = "file") %>% 
+# map read_csv() over file_name vector to get lookup tables into a list,
+# set_names() using column from form_IDS, bind_rows() can take a list of
+# dataframes as 1st argument and return single dataframe, with file column to
+# holding names of origin files.
+lookup <- file_name %>% map(~ suppressMessages(read_csv(here(
+  str_c("INPUT-FILES/OES-TABLES/", .x)
+)))) %>%
+  set_names(form_IDs$file) %>%
+  bind_rows(.id = "file") %>%
+# here we use left_join to bring in the cols from form_IDs, designating the
+# latter as input x to get those columns on the left side
+  left_join(x = form_IDs, by = "file") %>%
   select(-file) %>%
-  rename_with(
-    ~ str_replace_all( 
-      ., "_NT", ""
-    ),
-    VIS_NT:SOC_NT
-  ) %>% 
-  pivot_longer(
-    VIS:SOC,
-    names_to = "scale",
-    values_to = "t_score"
-  ) %>% 
-  relocate(scale, .after = "age_group") %>% 
+# rename_with() applies a renaming function (1st arg) to range of cols (2nd arg)
+    rename_with(~ str_replace_all(., "_NT", ""),
+              VIS_NT:SOC_NT) %>%
+  # The named score columns hold the t-scores corresponding to raw scores for
+  # the SPM-2 scales. We now pivot these cols into long format, such that raw-T
+  # lookup pairs are nested within scale names.
+  pivot_longer(VIS:SOC,
+               names_to = "scale",
+               values_to = "t_score") %>%
+  relocate(scale, .after = "age_group") %>%
+  # We sort the data hiearchically to match the desired row sequence, matching
+  # to sort orders contained in vectors pulled from form_IDs
   arrange(
-    match(form, all_of(form_order)), 
-    match(environ, all_of(environ_order)), 
-    match(age_group, all_of(age_group_order)), 
+    match(form, unique(form_IDs$form)),
+    match(environ, unique(form_IDs$environ)),
+    match(age_group, unique(form_IDs$age_group)),
     match(scale, all_of(scale_order)),
     desc(raw)
-    ) %>% 
-  drop_na(t_score) %>% 
+  ) %>%
+  # Because the subscales and total score have different raw score ranges, there
+  # are many raw score rows with no associated t-score pair (i.e., high raw
+  # scores are "off the scale" of the subscale scores). A simple way to drop
+  # these rows is drop_na() for the t-score col
+  drop_na(t_score) %>%
+  # here we substitue desired scale acronyms using mutate() and across()
   mutate(across(
     scale,
-    ~ case_when(
-      .x == "TS" ~ "T&S",
-      .x == "TOT" ~ "ST",
-      .x == "PLA" ~ "PLN",
-      T ~ .x
+    ~ case_when(.x == "TS" ~ "T&S",
+                .x == "TOT" ~ "ST",
+                .x == "PLA" ~ "PLN",
+                T ~ .x)
+  )) %>%
+  # we complete the table, adding percentile, min age, and max age cols using
+  # left_join() look ups, and an interp_range col using mutate() and case_when()
+  left_join(percentile, by = "t_score") %>%
+  left_join(min_max, by = "age_group") %>%
+  mutate(
+    interp_range = case_when(
+      t_score <= 59 ~ "Typical",
+      between(t_score, 60, 69) ~ "Moderate Difficulties",
+      t_score >= 70 ~ "Severe Difficulties"
     )
-  )) %>% 
-  left_join(percentile, by = "t_score") %>% 
-  left_join(min_max, by = "age_group") %>% 
-  mutate(interp_range = case_when(
-    t_score <=59 ~ "Typical", 
-    between(t_score, 60, 69) ~ "Moderate Difficulties",
-    t_score >= 70 ~ "Severe Difficulties"
-  ))
+  )
 
 file_name_school_environ <- map_chr(form_IDs_school_environ$file, ~ str_c(.x, "-cutoff-summary.csv"))
 
-lookup_school_environ <- file_name_school_environ %>% map(
-  ~ suppressMessages(read_csv(
-    here(
-      str_c("OUTPUT-FILES/SCHOOL-ENVIRON-DRIVING/", .x)
-    )
-  ))
-) %>% 
-  set_names(form_IDs_school_environ$file) %>% 
-  bind_rows(.id = "file") %>% 
-  select(file, TOT_raw, cutoff) %>% 
-  group_by(file) %>% 
-  mutate(
-    across(cutoff, 
-    ~ runner::fill_run(.)
-    )
-  ) %>% 
-  left_join(x = form_IDs_school_environ, by = "file") %>% 
-  rename(
-    raw = TOT_raw,
-    scale = file
-    ) %>% 
-  left_join(min_max, by = "age_group") %>% 
+lookup_school_environ <- file_name_school_environ %>% map(~ suppressMessages(read_csv(here(
+  str_c("OUTPUT-FILES/SCHOOL-ENVIRON-DRIVING/", .x)
+)))) %>%
+  set_names(form_IDs_school_environ$file) %>%
+  bind_rows(.id = "file") %>%
+  select(file, TOT_raw, cutoff) %>%
+  complete(file, TOT_raw = 15:60) %>%
+  group_by(file) %>%
+  mutate(across(cutoff,
+                ~ runner::fill_run(.))) %>%
+  left_join(x = form_IDs_school_environ, by = "file") %>%
+  rename(raw = TOT_raw,
+         scale = file) %>%
+  left_join(min_max, by = "age_group") %>%
   mutate(
     t_score = NA_real_,
     percentile = NA_real_,
-    interp_range = case_when(
-    cutoff == "cutoff" ~ "above cutoff", 
-    T ~ "below cutoff"
-  )) %>%
-  select(-cutoff) %>% 
-  relocate(scale, .after = "age_group") %>% 
-  relocate(c(t_score, percentile), .after = "raw")
+    interp_range = case_when(cutoff == "cutoff" ~ "above cutoff",
+                             T ~ "below cutoff")
+  ) %>%
+  select(-cutoff) %>%
+  relocate(scale, .after = "age_group") %>%
+  relocate(c(t_score, percentile), .after = "raw") %>% 
+  arrange(
+    match(environ, form_IDs_school_environ$environ),
+    desc(raw)
+  )
   
+file_name_driving <- map_chr(form_IDs_driving$file, ~ str_c(.x, "-cutoff-summary.csv"))
+
+lookup_driving <- file_name_driving %>% map(~ suppressMessages(read_csv(here(
+  str_c("OUTPUT-FILES/SCHOOL-ENVIRON-DRIVING/", .x)
+)))) %>%
+  set_names(form_IDs_driving$file) %>%
+  bind_rows(.id = "file") %>%
+  select(file, TOT_raw, cutoff) %>%
+  complete(file, TOT_raw = 18:72) %>%
+  group_by(file) %>%
+  mutate(across(cutoff,
+                ~ runner::fill_run(.))) %>%
+  left_join(x = form_IDs_driving, by = "file") %>%
+  rename(raw = TOT_raw,
+         scale = file) %>%
+  left_join(min_max, by = "age_group") %>%
+  mutate(
+    t_score = NA_real_,
+    percentile = NA_real_,
+    interp_range = case_when(cutoff == "cutoff" ~ "above cutoff",
+                             T ~ "below cutoff")
+  ) %>%
+  select(-cutoff) %>%
+  relocate(scale, .after = "age_group") %>%
+  relocate(c(t_score, percentile), .after = "raw") %>% 
+  arrange(
+    match(age_group, unique(form_IDs$age_group)),
+    match(environ, unique(form_IDs$environ)),
+    desc(raw)
+  )
   
+lookup_all <- bind_rows(lookup,
+                        lookup_school_environ,
+                        lookup_driving)
 
-
+ write_csv(
+   lookup_all,
+   here(
+     "OUTPUT-FILES/SCHOOL-ENVIRON-DRIVING/SPM2-OES-norms-lookup.csv"
+   ),
+   na = ""
+ )
  
--- CODE/scratch.R
+++ CODE/scratch.R
@@ -4,7 +4,15 @@ suppressMessages(library(tidyverse))
   
   
  
-test <- tibble(TOT_raw = 15:60) %>% 
+test_LJ <- tibble(TOT_raw = 15:60) %>% 
   left_join(lookup_school_environ[[1]], by = "TOT_raw")
 
+test_complete <- lookup_school_environ[[1]] %>%
+  mutate(file = "ART") %>% 
+  # relocate(file, .before = "TOT_raw") %>% 
+  complete(file, TOT_raw = 15:60)
 
+
+form1 <- unique(form_IDs$form)
+environ1 <- unique(form_IDs$environ)
+age_group1 <- unique(form_IDs$age_group)

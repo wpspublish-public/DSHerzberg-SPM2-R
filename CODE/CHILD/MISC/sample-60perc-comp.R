23715ac97ec3c9bb0b1999ffb5fe08886888972e
David Herzberg <dherzberg@wpspublish.com>
2021-05-04 17:56:28 -0700

    use dplyr::sample_frac() to obtain 60% sample of child-home normative sample

--- CODE/CHILD/MISC/sample-60perc-comp.R
+++ CODE/CHILD/MISC/sample-60perc-comp.R
@@ -2,16 +2,21 @@ suppressMessages(library(here))
 suppressMessages(suppressWarnings(library(tidyverse)))
 library(ggrepel) # ggplot2 EXTENSIONS
 
+# read in final normative sample for child-home form.
 Child_512_Home_desamp <- read_csv(
   here(
     "INPUT-FILES/CHILD/ALLDATA-DESAMP-NORMS-INPUT/CHILD-512-Home-allData-desamp.csv"
     ))
 
+# use dplyr::sample_frac() to get a 60% sample. By grouping on all demo vars, we
+# roughly preserve demographic proportions in smaller sample.
 set.seed(1234)
 sample_60perc <- Child_512_Home_desamp %>% 
   group_by(Age, Gender, ParentHighestEducation, Ethnicity, Region) %>%
   sample_frac(0.6)
 
+# quick check on counts/distribution for each demo variables, across full sample
+# and 60% sample.
 table(Child_512_Home_desamp$Age)
 table(sample_60perc$Age)
 
@@ -27,4 +32,8 @@ table(sample_60perc$Ethnicity)
 table(Child_512_Home_desamp$Region)
 table(sample_60perc$Region)
 
+# write .csv of 60% sample for use in other procedures
+write_csv(sample_60perc, here(
+  "INPUT-FILES/CHILD/ALLDATA-DESAMP-NORMS-INPUT/CHILD-512-Home-allData-desamp-60perc.csv"
+))
 
